12bb3c0c5190978e064f7be9251ca26331e79047
dymafr <erwan1309@hotmail.com>
2022-06-23 16:31:14 +0200

    1 file modified

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function EditTodo() {
-  return <h2>EditTodo</h2>;
+export default function EditTodo({ todo, saveTodo, cancelEditTodo }) {
+  const [value, setValue] = useState(todo.content);
+
+  function handleChange(e) {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === 'Enter' && value.length) {
+      saveTodo(value);
+      setValue('');
+    }
+  }
+
+  function handleClick() {
+    if (value.length) {
+      saveTodo(value);
+      setValue('');
+    }
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center mb-20">
+      <input
+        type="text"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        value={value}
+        className="mr-15 flex-fill"
+        placeholder="Ajouter une tâche"
+      />
+      <button onClick={handleClick} className="btn btn-primary">
+        Editer
+      </button>
+      <button onClick={cancelEditTodo} className="btn btn-reverse-primary">
+        Annuler
+      </button>
+    </div>
+  );
 }
